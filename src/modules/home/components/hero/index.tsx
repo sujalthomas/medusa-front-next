abe9f200ba7e4b382a0da5af44104689c3757504
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2024-03-24 11:09:00 -0700

    try

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,22 +1,35 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from "next/image"
+
+const MyImage = () => {
+  return (
+    <Image
+      src="/twitter-image.jpg"
+      alt="Hero image"
+      layout="fill"
+      objectFit="cover"
+    />
+  )
+}
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle"
+    style={{ backgroundImage: "/hero.jpg", backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Hemp - commerce Starter Template
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            Powered by Sujal Thomas and Next.js
           </Heading>
         </span>
         <a
